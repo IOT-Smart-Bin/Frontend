bf14814d35064a10347900c9720d7aea9b8dc4ea
SupakornSJB <limit4break@gmail.com>
2023-09-25 18:38:50 +0700

    change the look of the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import { Outlet, useNavigate } from "react-router-dom";
-import { Button, Navbar, Offcanvas } from "react-bootstrap";
-import './App.css'
+import { useState } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Button, Navbar, Offcanvas, Nav } from "react-bootstrap";
+import "./App.css";
+
+const routeThatCanBeUsedInNavbar = [
+  {name: "Home", route: "/", icon:"search-outline"},
+  {name: "Watchlist", route: "/watchlist", icon:"eye-outline"},
+  {name: "Map", route: "/map", icon:"map-outline"},
+]
 
 function App() {
   const [showNavbar, setNavbar] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
   /**
    *
@@ -22,7 +29,14 @@ function App() {
     <>
       <div className="page-container">
         <Navbar className="bg-body-secondary">
-          <Button onClick={() => setNavbar(true)}>Nav</Button>
+          <Button
+            onClick={() => setNavbar(true)}
+            as="button"
+            className="center ml-10"
+          >
+            <ion-icon name="menu-outline"></ion-icon>
+          </Button>
+          <p className="nav-title-text">Trash App</p>
         </Navbar>
         <Offcanvas
           show={showNavbar}
@@ -30,29 +44,23 @@ function App() {
           backdrop="static"
         >
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Trash App</Offcanvas.Title>
+            <div className="icon-and-tab-container">
+              <ion-icon name="trash-outline"></ion-icon>
+              <Offcanvas.Title>Trash App</Offcanvas.Title>
+            </div>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <div className="d-grid gap-2">
-              <Button
-                variant="outline-primary"
-                onClick={handleRouteChange("/")}
-              >
-                Home
-              </Button>
-              <Button
-                variant="outline-primary"
-                onClick={handleRouteChange("/watchlist")}
-              >
-                Watchlist
-              </Button>
-              <Button
-                variant="outline-primary"
-                onClick={handleRouteChange("/map")}
-              >
-                Map
-              </Button>
-            </div>
+            <Nav className="d-grid gap-2" as="div" fill >
+              {routeThatCanBeUsedInNavbar.map((routeObject) => (
+                <Button variant={location.pathname === routeObject.route ? "primary" : "secondary"}
+                    onClick={handleRouteChange(routeObject.route)}
+                    className="icon-and-tab-container"
+                  >
+                    <ion-icon name={routeObject.icon}></ion-icon>
+                    {routeObject.name}
+                </Button>
+              ))}
+            </Nav>
           </Offcanvas.Body>
         </Offcanvas>
         <div className="outlet-container">
