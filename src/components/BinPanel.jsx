f037fc8e5eb437fdf48ab7e9865092cb2f1ee8e6
ThanapatTr <116283565+ThanapatTr@users.noreply.github.com>
2023-09-25 17:43:21 +0700

    Bin Panel v2

--- src/components/BinPanel.jsx
+++ src/components/BinPanel.jsx
@@ -20,22 +20,26 @@ function BinPanel({ bid, tags, name, pictureLink, capacity, gas, weight, timesta
 
   return (
     <div className="square-box">
-      <div className="left-side">
-        <img
-          src={pictureLink || 'https://via.placeholder.com/200x200?text=Placeholder'}
-          alt={`${name} Image`}
-          width="100%"
-          height="100%"
-        />
+      <div className="top">
+        <h4><strong>{name ?? 'N/A'}</strong></h4>
       </div>
-      <div className="right-side">
-        <h2><strong>{name ?? 'N/A'}</strong></h2>
-        <p><strong>Tags:</strong> {tags?.join(', ') || 'N/A'}</p>
-        <p><strong>Capacity:</strong> {capacity ?? 'N/A'}%</p>
-        <p><strong>Gas Data:</strong> {gas ?? 'N/A'}%</p>
-        <p><strong>Timestamp:</strong> {timestamp ?? 'N/A'}</p>
+      <div className="bottom">
+        <div className="left-side">
+          <img
+            src={pictureLink || 'https://via.placeholder.com/200x200?text=Placeholder'}
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="right-side">
+          <p><strong>Tags:</strong> {tags?.join(', ') || 'N/A'}</p>
+          <p><strong>Capacity:</strong> {capacity ?? 'N/A'}%</p>
+          <p><strong>Gas:</strong> {gas ?? 'N/A'}</p>
+          <p><strong>Last updated:</strong> {timestamp ?? 'N/A'}</p>
+        </div>
       </div>
     </div>
+
   );
 }
 
