89b66fef6591ac85eb6f73617718a4c7c75c3b8c
ThanapatTr <116283565+ThanapatTr@users.noreply.github.com>
2023-09-27 21:24:52 +0700

    Implement Api request

--- src/pages/WatchlistPage/WatchlistPage.jsx
+++ src/pages/WatchlistPage/WatchlistPage.jsx
@@ -4,31 +4,40 @@ import "./watchlist-page.css";
 import BinPanel from '../../components/BinPanel/BinPanel';
 import mockData from '../../MockData'
 import { readBID, } from '../../util/localStorageReadWrite';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 function WatchlistPage() {
+  const isMountedRef = useRef(false);
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
 
   // get the data of bids in watchlist and store them in an array upon entering the page
   useEffect(() => {
+     // use to stop activating twice when the page is loaded
+    if (!isMountedRef.current) {
+      isMountedRef.current = true;
+    } else {
+      return;
+    }
     getWatchlistData();
-    console.log('hello 1')
   }, []);
 
   const getWatchlistData = async () => {
     setLoading(true);
     try {
-      const watchlistBids = readBID(); // array of string of bids
-      const url = 'endpoint';
-      const params = { watchlistBids };
-      //const response = await axios.get(url, { params });
-      const response = mockData;
-      if (Array.isArray(response) && response.length === 0) {
-        setResults([]);
+      const url = 'http://13.229.60.73:8000/bins';
+      const params = readBID(); // array of string of bids
+      const response = await axios.post(url, params);
+
+      if (Array.isArray(response.data) && response.data.length > 0) {
+        setResults(response.data);
+        console.log('bin found');
       } else {
-        setResults(response);
+        setResults([]);
+        console.log('bin not found');
+        console.log(response);
       }
+
     } catch (error) {
       console.error(error);
     } finally {
@@ -43,21 +52,21 @@ function WatchlistPage() {
           <p>There is currently no item in your watchlist.</p>
         ) : (
           <div className="gallery-container">
-            {results.map((result, index) => (
-              <div className="gallery-item" key={index}>
-                <BinPanel
-                  bid={result.bid}
-                  tags={result.tags}
-                  name={result.name}
-                  pictureLink={result.pictureLink}
-                  capacity={result.capacity}
-                  gas={result.gas}
-                  weight={result.weight}
-                  timestamp={result.timestamp}
-                />
-              </div>
-            ))}
-          </div>
+              {results.map((result, index) => (
+                <div className="gallery-item" key={index}>
+                  <BinPanel
+                    bid={result.bid}
+                    tags={result.tags}
+                    name={result.name}
+                    pictureLink={result.image} 
+                    capacity={result.latest_data_point?.capacity ?? 'N/A'}
+                    gas={result.latest_data_point?.gas ?? 'N/A'}
+                    weight={result.latest_data_point?.weight ?? 'N/A'}
+                    timestamp={result.latest_data_point?.timestamp ?? 'N/A'}
+                  />
+                </div>
+              ))}
+            </div>
         )}
       </div>
     </>
